31f8f2d2710599512411f10797ff1c8621fdbbca
skif-cezar <skif.cezar@gmail.com>
2023-07-20 20:41:28 +0300

    feat: add header component

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -8,10 +8,17 @@ import {Menu} from "src/app/components/menu/Menu";
  */
 export const Header: React.FC = () => {
   const HEADER_STYLES = clsx(styles.header);
+  const LOGO_STYLES = clsx(styles.logo);
+  const MAIL_STYLES = clsx(styles.mail);
 
   return (
     <header className={HEADER_STYLES}>
-      <div>Top</div>
+      <div className={LOGO_STYLES}>
+        <a href="/">BELLFAM</a>
+      </div>
+      <address className={MAIL_STYLES}>
+        <a href="mailto: Info@yourmail.com">Info@yourmail.com</a>
+      </address>
       <Menu />
     </header>
   );
